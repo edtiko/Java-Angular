6ffb5364985932e8da694acdfe076530fe423754
Andres Lopez <andres.lopez@expertla.com>
2016-08-03 08:53:29 -0500

    Corrección bugs

--- training/training-web/src/main/webapp/static/js/run-custom-training.js
+++ training/training-web/src/main/webapp/static/js/run-custom-training.js
@@ -3,8 +3,8 @@
 // Defines the javascript files that need to be loaded and their dependencies.
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-var $contextPath = "http://localhost:8085/training/";
-//var $contextPath = "http://181.143.227.220:8086/training/";
+//var $contextPath = "http://localhost:8085/training/";
+var $contextPath = "http://181.143.227.220:8086/training/";
 require.config({
     paths: {
         angular: 'lib/angular.min',
