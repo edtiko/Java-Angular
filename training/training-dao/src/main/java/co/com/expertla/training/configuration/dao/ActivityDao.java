8c08d594bf3024ebdd4eb42f61928d47949753ba
Andres Lopez <andres.lopez@expertla.com>
2016-08-05 16:27:48 -0500

    Se adicionan service de activity

--- training/training-dao/src/main/java/co/com/expertla/training/configuration/dao/ActivityDao.java
+++ training/training-dao/src/main/java/co/com/expertla/training/configuration/dao/ActivityDao.java
@@ -5,33 +5,72 @@ import co.com.expertla.training.model.entities.Activity;
 import java.util.List;
 
 /**
-* Dao for Activity <br>
-* Creation Date : <br>
-* date 21/07/2016 <br>
-* @author Angela Ramírez
-**/
-public interface ActivityDao extends BaseDAO<Activity>{
-	
+ * Activity Dao <br>
+ * Info. Creación: <br>
+ * fecha 5/08/2016 <br>
+ *
+ * @author Andres Felipe Lopez Rodriguez
+*
+ */
+public interface ActivityDao extends BaseDAO<Activity> {
+
     /**
-     * Trae todos los registros de activity <br>
-     * Creation Date : <br>
-     * date 21/07/2016 <br>
-     * @author Angela Ramírez
-     * @throws Exception
+     * Obtiene todos los registros de activity <br>
+     * Info. Creación: <br>
+     * fecha 5/08/2016 <br>
+     *
+     * @author Andres Felipe Lopez Rodriguez
      * @return
+     * @throws Exception
      */
     public List<Activity> findAll() throws Exception;
-    
+
     /**
-     * Trae todos los registros de Activity  por modality Id y objective Id<br>
+     * Obtiene todos los registros activos de activity <br>
+     * Info. Creación: <br>
+     * fecha 5/08/2016 <br>
+     *
+     * @author Andres Felipe Lopez Rodriguez
+     * @return
+     * @throws Exception
+     */
+    public List<Activity> findAllActive() throws Exception;
+
+    /**
+     * Obtiene todos los registros de activity por su id <br>
+     * Info. Creación: <br>
+     * fecha 5/08/2016 <br>
+     *
+     * @author Andres Felipe Lopez Rodriguez
+     * @param activity
+     * @return
+     * @throws Exception
+     */
+    public List<Activity> findByActivity(Activity activity) throws Exception;
+
+    /**
+     * Obtiene todos los registros de activity por el filtro del usuario <br>
+     * Info. Creación: <br>
+     * fecha 5/08/2016 <br>
+     *
+     * @author Andres Felipe Lopez Rodriguez
+     * @param activity
+     * @return
+     * @throws Exception
+     */
+    public List<Activity> findByFiltro(Activity activity) throws Exception;
+
+    /**
+     * Trae todos los registros de Activity por modality Id y objective Id<br>
      * Creation Date : <br>
      * date 21/07/2016 <br>
+     *
      * @author Angela Ramírez
      * @param objectiveId
      * @param modalityId
      * @throws Exception
      * @return
      */
-    public List<Activity> findByObjectiveIdAndModalityId(Integer objectiveId,Integer modalityId) throws Exception;
-        
+    public List<Activity> findByObjectiveIdAndModalityId(Integer objectiveId, Integer modalityId) throws Exception;
+
 }
--- training/training-dao/src/main/java/co/com/expertla/training/configuration/dao/impl/ActivityDaoImpl.java
+++ training/training-dao/src/main/java/co/com/expertla/training/configuration/dao/impl/ActivityDaoImpl.java
@@ -4,31 +4,60 @@ import co.com.expertla.base.jpa.BaseDAOImpl;
 import co.com.expertla.training.configuration.dao.ActivityDao;
 import co.com.expertla.training.model.entities.Activity;
 import java.util.List;
-
+import javax.persistence.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
-
 /**
-* Dao Implementation for Activity <br>
-* Creation Date : <br>
-* date 21/07/2016 <br>
-* @author Angela Ramírez
-**/
+ * Activity Dao Impl <br>
+ * Info. Creación: <br>
+ * fecha 5/08/2016 <br>
+ *
+ * @author Andres Felipe Lopez Rodriguez
+*
+ */
 @Repository
 public class ActivityDaoImpl extends BaseDAOImpl<Activity> implements ActivityDao {
 
+    public ActivityDaoImpl() {
+    }
+
     @Override
     public List<Activity> findAll() throws Exception {
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT a");
-        sql.append("FROM Activity a ");
-        Query query = getEntityManager().createQuery(sql.toString());
-        return query.getResultList();
+        StringBuilder builder = new StringBuilder();
+        builder.append("select a from Activity a ");
+        builder.append("order by a.ActivityId desc ");
+        return createQuery(builder.toString());
     }
-    
+
     @Override
-    public List<Activity> findByObjectiveIdAndModalityId(Integer objectiveId, Integer modalityId) throws Exception {      
+    public List<Activity> findAllActive() throws Exception {
+        StringBuilder builder = new StringBuilder();
+        builder.append("select a from Activity a ");
+
+//        setParameter("active", Status.ACTIVE.getId());
+        return createQuery(builder.toString());
+    }
+
+    @Override
+    public List<Activity> findByActivity(Activity activity) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        builder.append("select a from Activity a ");
+        builder.append("WHERE a.activityId = :id ");
+        setParameter("id", activity.getActivityId());
+        return createQuery(builder.toString());
+    }
+
+    @Override
+    public List<Activity> findByFiltro(Activity activity) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        builder.append("select a from Activity a ");
+        builder.append("WHERE 1=1 ");
+
+        return createQuery(builder.toString());
+    }
+
+    @Override
+    public List<Activity> findByObjectiveIdAndModalityId(Integer objectiveId, Integer modalityId) throws Exception {
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT a ");
         sql.append("FROM Activity a ");
