4826204b2027c11fb90462204f9a25ea661d933a
Andres Lopez <andres.lopez@expertla.com>
2016-08-31 10:46:06 -0500

    Corrección merge

---
+++ training/training-dao/src/main/java/co/com/expertla/training/dao/security/RoleDao.java
@@ -0,0 +1,72 @@
+package co.com.expertla.training.dao.security;
+
+import co.com.expertla.base.jpa.BaseDAO;
+import co.com.expertla.training.model.dto.RoleDTO;
+import co.com.expertla.training.model.entities.Role;
+import java.util.List;
+
+/**
+* Role Dao <br>
+* Info. Creación: <br>
+* fecha 30/08/2016 <br>
+* @author Andres Felipe Lopez Rodriguez
+**/
+public interface RoleDao extends BaseDAO<Role> {
+    
+    
+    /**
+     * Obtiene todos los registros de role <br>
+     * Info. Creación: <br>
+     * fecha 30/08/2016 <br>
+     * @author Andres Felipe Lopez Rodriguez
+     * @return
+     * @throws Exception 
+     */
+    public List<Role> findAll() throws Exception;
+    
+    /**
+     * Obtiene todos los registros activos de role <br>
+     * Info. Creación: <br>
+     * fecha 30/08/2016 <br>
+     * @author Andres Felipe Lopez Rodriguez
+     * @return
+     * @throws Exception 
+     */
+    public List<Role> findAllActive() throws Exception;
+
+    /**
+     * Obtiene todos los registros de role paginados <br>
+     * Info. Creación: <br>
+     * fecha 30/08/2016 <br>
+     * @author Andres Felipe Lopez Rodriguez
+     * @param first
+     * @param max
+     * @param order
+     * @return
+     * @throws Exception 
+     */
+    public List<RoleDTO> findPaginate(int first, int max, String order) throws Exception;
+
+    /**
+     * Obtiene todos los registros de role por su id <br>
+     * Info. Creación: <br>
+     * fecha 30/08/2016 <br>
+     * @author Andres Felipe Lopez Rodriguez
+     * @param role
+     * @return
+     * @throws Exception 
+     */
+    public List<Role> findByRole(Role role) throws Exception;   
+
+    /**
+     * Obtiene todos los registros de role por el filtro del usuario <br>
+     * Info. Creación: <br>
+     * fecha 30/08/2016 <br>
+     * @author Andres Felipe Lopez Rodriguez
+     * @param role
+     * @return
+     * @throws Exception 
+     */
+    public List<Role> findByFiltro(Role role) throws Exception; 
+
+}
