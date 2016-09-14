836372ed4aba9be9866a03b43026c099eed09a96
Andres Lopez <andres.lopez@expertla.com>
2016-09-14 15:44:29 -0500

    no message

--- training/training-web/src/main/java/co/com/expertla/training/web/controller/security/ModuleController.java
+++ training/training-web/src/main/java/co/com/expertla/training/web/controller/security/ModuleController.java
@@ -1,20 +1,25 @@
 package co.com.expertla.training.web.controller.security;
 
 import co.com.expertla.base.util.MessageUtil;
+import co.com.expertla.training.enums.StateEnum;
 import co.com.expertla.training.enums.Status;
 import co.com.expertla.training.model.dto.ModuleDTO;
 import co.com.expertla.training.model.dto.PaginateDto;
 import co.com.expertla.training.model.entities.Module;
+import co.com.expertla.training.model.entities.Option;
 import java.util.List;
 import co.com.expertla.training.model.util.ResponseService;
 import co.com.expertla.training.service.security.ModuleService;
+import co.com.expertla.training.service.security.OptionService;
 import co.com.expertla.training.web.enums.StatusResponse;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.logging.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +38,10 @@ public class ModuleController {
 
     @Autowired
     ModuleService moduleService;
+    
+    @Autowired
+    OptionService optionService;
+
 
     /**
      * Crea module <br>
@@ -159,6 +168,43 @@ public class ModuleController {
         try {
             List<Module> moduleList = moduleService.findAllActive();
             responseService.setOutput(moduleList);
+            responseService.setStatus(StatusResponse.SUCCESS.getName());
+            return new ResponseEntity<>(responseService, HttpStatus.OK);
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(ModuleController.class.getName()).log(Level.SEVERE, null, ex);
+            responseService.setOutput("Error al consultar module");
+            responseService.setDetail(ex.getMessage());
+            responseService.setStatus(StatusResponse.FAIL.getName());
+            return new ResponseEntity<>(responseService, HttpStatus.OK);
+        }
+    }
+    
+    /**
+     * Consulta modulos por usuario <br>
+     * Info. Creacion: <br>
+     * fecha 14/09/2016 <br>
+     *
+     * @author Andres Felipe Lopez Rodriguez
+     * @param userId
+     * @return
+     */
+    @RequestMapping(value = "/module/get/by/userId/{userId}", method = RequestMethod.GET)
+    public ResponseEntity<ResponseService> listModuleByUser(@PathVariable("userId") Integer userId) {
+        ResponseService responseService = new ResponseService();
+        try {
+            List<Module> moduleList = moduleService.findByUserId(userId);
+            List<ModuleDTO> moduleDTOList = new ArrayList();
+            for (Module module : moduleList) {
+                ModuleDTO moduleDTO = new ModuleDTO();
+                moduleDTO.setModuleId(module.getModuleId());
+                moduleDTO.setName(module.getName());
+                Option option = new Option();
+                option.setModuleId(module);
+                option.setStateId(Short.valueOf(StateEnum.ACTIVE.getId().toString()));
+                moduleDTO.setOptionCollection(optionService.findByFiltro(option));
+                moduleDTOList.add(moduleDTO);
+            }
+            responseService.setOutput(moduleDTOList);
             responseService.setStatus(StatusResponse.SUCCESS.getName());
             return new ResponseEntity<>(responseService, HttpStatus.OK);
         } catch (Exception ex) {
