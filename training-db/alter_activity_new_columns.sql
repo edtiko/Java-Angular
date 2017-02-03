9bb117cf19e12367f392b964e39a851ebb4c884c
Edwin Gomez <edwin.gomez@expertla.com>
2017-02-03 16:17:31 -0500

    ajustes de letra y visuales

--- training-db/alter_activity_new_columns.sql
+++ training-db/alter_activity_new_columns.sql
@@ -3,3 +3,8 @@ ALTER TABLE activity ADD COLUMN state_id integer, ADD COLUMN creation_date times
 ADD COLUMN user_create integer, ADD COLUMN user_update integer;
 ALTER TABLE physiological_capacity ADD COLUMN state_id integer, ADD COLUMN creation_date timestamp without time zone, ADD COLUMN last_update timestamp without time zone,
 ADD COLUMN user_create integer, ADD COLUMN user_update integer;
+
+tipo de letra numeros
+si el dashboard va a ocupar la resolución por defecto o va tener scroll
+tamaños de fuente
+como se muestra las notificaciones de la campanita (diseño)
