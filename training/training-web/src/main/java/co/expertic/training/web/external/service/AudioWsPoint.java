869e2b8c6bdc25542fe5670bd6b61fa36baa065f
Edwin Gomez <edwin.gomez@expertla.com>
2017-02-06 14:23:04 -0500

    ajuste mapping

--- training/training-web/src/main/java/co/expertic/training/web/external/service/AudioWsPoint.java
+++ training/training-web/src/main/java/co/expertic/training/web/external/service/AudioWsPoint.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.socket.server.standard.SpringConfigurator;
 
-@ServerEndpoint(value = "/audio/{sessionId}",
+@ServerEndpoint(value = "/audiows/{sessionId}",
         configurator = SpringConfigurator.class,
         encoders = AudioEncoder.class,
         decoders = AudioDecoder.class)
